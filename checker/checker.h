80b6151adf91c0e5aaf3c8d4b73a1c662c3551d8
GJSteyn <36443537+GJSteyn@users.noreply.github.com>
2018-06-30 17:27:51 +0200

    Add checker and push_swap header files

---
+++ checker/checker.h
@@ -0,0 +1,9 @@
+#ifndef CHECKER_H
+# define CHECKER_H
+
+# include "libft.h"
+# include "get_next_line.h"
+# include <unistd.h>
+# include <stdlib.h>
+
+#endif
---
+++ push_swap/push_swap.h
@@ -0,0 +1,9 @@
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include "libft.h"
+# include "get_next_line.h"
+# include <unistd.h>
+# include <stdlib.h>
+
+#endif
