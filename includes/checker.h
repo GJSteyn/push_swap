46fbc58bc167288cdc9c66f3e8431f69b115441d
GJSteyn <6443537+GJSteyn@users.noreply.github.com>
2018-07-03 11:59:55 +0200

    Add check for only integer arguments

--- includes/checker.h
+++ includes/checker.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   checker.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gsteyn <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/07/03 11:30:19 by gsteyn            #+#    #+#             */
+/*   Updated: 2018/07/03 11:30:33 by gsteyn           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef CHECKER_H
 # define CHECKER_H
 
@@ -5,5 +17,8 @@
 # include "get_next_line.h"
 # include <unistd.h>
 # include <stdlib.h>
+
+int					args_valid(int arc, char **arv);
+int					args_are_ints(int arc, char **arv);
 
 #endif
--- srcs/check_args.c
+++ srcs/check_args.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_args.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gsteyn <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/07/03 11:29:03 by gsteyn            #+#    #+#             */
+/*   Updated: 2018/07/03 11:53:28 by gsteyn           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 #include "checker.h"
 #include "push_swap.h"
@@ -9,8 +21,25 @@ int		args_valid(int arc, char **arv)
 	i = 1;
 	while (i < arc)
 	{
-		if (!ft_only_digits(arv[i]))
+		if (!ft_only_digits(arv[i]) || !args_are_ints()
 			return (0);
 	}
 	return (1);
 }
+
+int		args_are_ints(int arc, char **arv)
+{
+	int			i;
+	long		result;
+
+	i = 0;
+	while (++i < arc)
+	{
+		result = ft_atoi(arv[i]);
+		if (result > 2147483647 || result < -2147483648)
+			return (0);
+	}
+	return (1);
+}
+
+int		no_duplicates(
