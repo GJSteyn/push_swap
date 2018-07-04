7965b6a0569af4b9f908c8df4f8da53bb9276165
GJSteyn <36443537+GJSteyn@users.noreply.github.com>
2018-07-04 16:48:55 +0200

    Add descriptions to pop and push list functions

--- srcs/list_ops.c
+++ srcs/list_ops.c
@@ -6,7 +6,7 @@
 /*   By: gsteyn <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/03 11:57:13 by gsteyn            #+#    #+#             */
-/*   Updated: 2018/07/04 08:00:58 by gsteyn           ###   ########.fr       */
+/*   Updated: 2018/07/04 16:47:51 by gsteyn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,11 @@ void		lst_swap(t_list **lst)
 	}
 }
 
+/* Pop:
+ * Removes the first item in a list and returns it.
+ * The second item, if it exists, becomes the new top.
+ */
+
 t_list		*lst_pop(t_list **lst)
 {
 	t_list		*ret;
@@ -40,6 +45,12 @@ t_list		*lst_pop(t_list **lst)
 	}
 	return (NULL);
 }
+
+/* Push:
+ * Take the first element at the top of a list and
+ * put it at the top of another. Do nothing if the
+ * source list is empty.
+ */
 
 void		lst_push(t_list **dst, t_list **src)
 {
