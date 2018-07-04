a285294779d8bf03376caa8a442aa078d071b5ac
GJSteyn <6443537+GJSteyn@users.noreply.github.com>
2018-07-04 06:19:47 +0200

    Add list_ops and first method ('swap')

--- srcs/list_ops.c
+++ srcs/list_ops.c
@@ -1,24 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.h                                        :+:      :+:    :+:   */
+/*   list_ops.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gsteyn <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/07/03 11:30:02 by gsteyn            #+#    #+#             */
-/*   Updated: 2018/07/03 13:48:13 by gsteyn           ###   ########.fr       */
+/*   Created: 2018/07/03 11:57:13 by gsteyn            #+#    #+#             */
+/*   Updated: 2018/07/03 13:49:29 by gsteyn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PUSH_SWAP_H
-# define PUSH_SWAP_H
+#include "libft.h"
+#include "push_swap.h"
 
-# include "libft.h"
-# include "get_next_line.h"
-# include <unistd.h>
-# include <stdlib.h>
-# include <string.h>
+/* Swap:
+ * Swap the first two element at the top of a stack.
+ * Do nothing if there is only one or no elements.
+ */
 
-t_list				*swap(t_list *lst);
+t_list		*swap(t_list *lst)
+{
+	if (lst && lst->next)
+	{
+		lst->next = lst->next->next;
+		lst->next->next = lst;
+	}
+}
 
-#endif
+t_list		*pop(t_list *dest, t_list *src)
+{
+
+}
