20d6b3faa68d131c39c08079ef89aa1d03cd16e3
GJSteyn <6443537+GJSteyn@users.noreply.github.com>
2018-07-09 06:54:31 +0200

    Modify get_args to parse strings with multiple ints

--- srcs/get_args.c
+++ srcs/get_args.c
@@ -6,7 +6,7 @@
 /*   By: gsteyn <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/09 05:23:14 by gsteyn            #+#    #+#             */
-/*   Updated: 2018/07/09 05:23:19 by gsteyn           ###   ########.fr       */
+/*   Updated: 2018/07/09 06:51:50 by gsteyn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,37 @@
  * returns them in a list with integers as content.
  */
 
+t_list		*get_args(int arc, char **arv)
+{
+	t_list		*ret;
+	t_list		*tmp;
+	char		**split;
+	int			i;
+	int			j;
+	int			curr;
+
+	i = 1;
+	j = -1;
+	while (i < arc)
+	{
+		split = ft_strsplit(arv[i], ' ');
+		while (split[++j])
+		{
+			curr = ft_atoi(split[j]);
+			if (!ret)
+				ret = ft_lstnew(&curr, sizeof(int));
+			else
+			{
+				tmp = ft_lstnew(&curr, sizeof(int));
+				lst_append(ret, tmp);
+			}
+		}
+		ft_strldel(&split);
+	}
+	return (ret);
+}
+
+/*	Old get_args. Might still be useful?
 t_list		*get_args(int arc, char **arv)
 {
 	t_list		*ret;
@@ -36,3 +67,4 @@ t_list		*get_args(int arc, char **arv)
 	}
 	return (ret);
 }
+*/
