0b0781e146a569121c979dcbba684212b39b75d9
GJSteyn <6443537+GJSteyn@users.noreply.github.com>
2018-07-10 07:18:03 +0200

    Remove old get_args function

--- srcs/get_args.c
+++ srcs/get_args.c
@@ -6,7 +6,7 @@
 /*   By: gsteyn <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/09 05:23:14 by gsteyn            #+#    #+#             */
-/*   Updated: 2018/07/09 17:36:55 by gsteyn           ###   ########.fr       */
+/*   Updated: 2018/07/10 07:17:45 by gsteyn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,24 +47,3 @@ t_list		*get_args(int arc, char **arv)
 	}
 	return (ret);
 }
-
-/*	Old get_args. Might still be useful?
-t_list		*get_args(int arc, char **arv)
-{
-	t_list		*ret;
-	t_list		*tmp;
-	int			curr;
-	int			i;
-
-	curr = ft_atoi(arv[1]);
-	ret = ft_lstnew(&curr, sizeof(int));
-	i = 1;
-	while (++i < arc)
-	{
-		curr = ft_atoi(arv[i]);
-		tmp = ft_lstnew(&curr, sizeof(int));
-		lst_append(ret, tmp);
-	}
-	return (ret);
-}
-*/
