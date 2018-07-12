20d1ce30263f03882bfd6c28362ed0e58dabdc84
GJSteyn <6443537+GJSteyn@users.noreply.github.com>
2018-07-12 07:40:58 +0200

    Incorporate normalization of arguments in push_swap.c

--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: gsteyn <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/03 11:29:23 by gsteyn            #+#    #+#             */
-/*   Updated: 2018/07/11 18:43:20 by gsteyn           ###   ########.fr       */
+/*   Updated: 2018/07/12 07:35:15 by gsteyn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ int		main(int arc, char **arv)
 {
 	//int		i;
 	t_list		*args;
+	t_list		*sargs;
 	t_list		*stack_b;
 	t_s_hold	*stacks;
 
@@ -33,6 +34,8 @@ int		main(int arc, char **arv)
 		ft_putstr_fd("Error\n--Duplicates\n", 2);
 		return (0);
 	}
+	sargs = get_sorted_args(arc, arv);
+	normlist(args, sargs);
 	stacks = sh_init(args, stack_b);
 	push_and_swap(stacks);
 	return (0);
