ad325340cc83bb8e2f4157840ccb3c6d8e9de70c
GJSteyn <6443537+GJSteyn@users.noreply.github.com>
2018-07-17 17:44:08 +0200

    Improve sort4 function

--- srcs/merge.c
+++ srcs/merge.c
@@ -6,7 +6,7 @@
 /*   By: gsteyn <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/16 05:59:56 by gsteyn            #+#    #+#             */
-/*   Updated: 2018/07/16 16:31:07 by gsteyn           ###   ########.fr       */
+/*   Updated: 2018/07/17 09:11:28 by gsteyn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "push_swap.h"
@@ -41,6 +41,7 @@ static int	get_closest_dir(t_list *lst, int target, int lstlen)
 	return (best_ret);
 }
 
+/*
 static void	put_back(t_s_hold *st)
 {
 	int		len;
@@ -66,6 +67,62 @@ static void	put_back(t_s_hold *st)
 		len--;
 	}
 }
+*/
+
+static int 	get_closest_dir2(t_list *lst, int target)
+{
+	int		best;
+	int		best_ret;
+	int		i;
+	int		curr;
+	int		half;
+
+	if (*(int*)lst->content == target)
+		return (0);
+	i = 0;
+	best = ft_lstlen(lst);
+	half = best / 2;
+	while (lst)
+	{
+		if (i > half)
+			curr = i - target;
+		else
+			curr = i;
+		if (*(int*)lst->content == target && ft_abs(curr) < best)
+		{
+			best = ft_abs(curr);
+			best_ret = curr;
+		}
+		i++;
+		lst = lst->next;
+	}
+	return (best_ret);
+}
+
+static void	put_back2(t_s_hold *st, int size)
+{
+	int		side;
+
+	while (size > 0)
+	{
+		side = get_closest_dir2(st->stack_b, size);
+		while (side != 0)
+		{
+			if (side < 0)
+			{
+				rev_rotate_b(st);
+				side++;
+			}
+			else if (side > 0)
+			{
+				rotate_b(st);
+				side--;
+			}
+		}
+		push_a(st);
+		size--;
+	}
+}
 
 void		sort4(t_s_hold *st)
 {
@@ -74,30 +131,24 @@ void		sort4(t_s_hold *st)
 	int		curr;
 	int		i;
 	int		side;
+	int		blocksize;
 
+	blocksize = 21;
 	len = ft_lstlen(st->stack_a);
-	curr = 25;
+	curr = blocksize;
 	while (1)
 	{
 		len2 = ft_lstlen(st->stack_a);
 		i = 0;
-		while (i < 25 && len2 > 0)
+		while (i < blocksize && len2 > 0)
 		{
 			side = get_closest_dir(st->stack_a, curr, len);
 			if (side > 0)
-			{
 				while (*(int*)st->stack_a->content > curr)
-				{
 					rotate_a(st);
-				}
-			}
-			else if (side < 0)
-			{
+			else if (side < blocksize)
 				while (*(int*)st->stack_a->content > curr)
-				{
 					rev_rotate_a(st);
-				}
-			}
 			push_b(st);
 			i++;
 			if (get_first(st->stack_b) < get_second(st->stack_b))
@@ -106,10 +157,10 @@ void		sort4(t_s_hold *st)
 		}
 		if (curr == len)
 			break ;
-		else if (curr < len - 25)
-			curr += 25;
+		else if (curr < len - blocksize)
+			curr += blocksize;
 		else
 			curr += len - curr;
 	}
-	put_back(st);
+	put_back2(st, len);
 }
