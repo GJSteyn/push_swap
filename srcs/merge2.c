0a16840b129b1feb6dbb4ce1acb75143790cba08
GJSteyn <6443537+GJSteyn@users.noreply.github.com>
2018-07-21 16:42:40 +0200

    Unimportant changes to merge2.c

--- srcs/merge2.c
+++ srcs/merge2.c
@@ -1,11 +1,16 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   merge2.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gsteyn <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/07/21 16:41:01 by gsteyn            #+#    #+#             */
+/*   Updated: 2018/07/21 16:41:17 by gsteyn           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
-
-
-		if (ft_lstlen(st->stack_a) < curr * 2)
-		{
-			sort_a(st);
-			merge_to_b(st, ft_lstlen(st->stack_a));
-		}
 
 static void	sort_a_top2(t_s_hold *st)
 {
@@ -100,8 +105,3 @@ void		merge_sort(t_s_hold *st)
 		//merge_to_b(st, curr, curr);
 	}
 }
-
-// To do: Instead of legit merge sorting, first try and improve the previous merge
-// algorithm by reading the values back to a more efficiently. Do this by reading
-// them to a in much the same way you read them to b. Find the next one and the
-// closest path to it, rotate there and push it back.
